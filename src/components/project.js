c363d6223a4b2288819f6673d5067314167f7064
kshithija147@gmail.com <kshithija147@gmail.com>
2020-05-25 22:23:30 -0700

    modified project

--- src/components/project.js
+++ src/components/project.js
@@ -1,9 +1,53 @@
 import React, { Component } from 'react';
+import { Tabs, Tab } from 'react-mdl';
 
 class Project extends Component{
+
+    constructor(props){
+        super(props);
+        this.state ={activeTab:0};
+    }
+        toggleCategories(){
+            if(this.state.activeTab === 0){
+                return(
+                    <div><h1>This is React</h1></div>
+                 )
+                } 
+            else if(this.state.activeTab === 1){
+                    return(
+                        <div><h1>This is Angular</h1></div>
+                     )
+                    } 
+            else if(this.state.activeTab === 2){
+                        return(
+                            <div><h1>This is VueJs</h1></div>
+                         )
+                        }  
+            else if(this.state.activeTab === 3){
+                            return(
+                                <div><h1>This is MongoDB</h1></div>
+                             )
+                            }  
+        }
+       
     render () {
         return(
-            <div><h1>Project page</h1></div>
+
+            <div className="category-tabs" >
+                <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({activeTab: tabId}) } ripple>
+                <Tab>React</Tab>
+                <Tab>Angular</Tab>
+                <Tab>VueJs</Tab>
+                <Tab>MongoDB</Tab>
+
+                </Tabs>
+
+                <section className="project-grid">
+                    {this.toggleCategories()}
+                </section>
+
+            </div>
+            
         )
             
         
